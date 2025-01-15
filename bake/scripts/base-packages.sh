5c21df825eedcacbfe40d64d72daa9cc33b0f07b
Abigail Ranson <abby@abbyranson.com>
2025-01-15 14:23:09 -0800

    Add jq

--- bake/scripts/base-packages.sh
+++ bake/scripts/base-packages.sh
@@ -2,4 +2,4 @@
 set -ex
 echo 'Installing base packages...'
 apk update
-apk add --no-cache bash wget curl git make python3 py3-pip nodejs npm
+apk add --no-cache bash wget curl git make python3 py3-pip nodejs npm jq
