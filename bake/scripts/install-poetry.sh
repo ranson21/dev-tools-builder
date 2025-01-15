a129a412acc3568e2b64feb28574712f8cf0cea8
Abigail Ranson <abby@abbyranson.com>
2025-01-14 16:59:34 -0800

    Fix poetry path

--- bake/scripts/install-poetry.sh
+++ bake/scripts/install-poetry.sh
@@ -2,5 +2,5 @@
 set -ex
 echo 'Installing Poetry...'
 cd /download
-curl -sSL https://install.python-poetry.org | python3 -
+curl -sSL https://install.python-poetry.org | POETRY_HOME=/usr/local python3 -
 echo 'export PATH="/root/.local/bin:$PATH"' >> /root/.bashrc
